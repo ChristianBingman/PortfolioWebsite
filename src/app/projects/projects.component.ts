6ee08987066fde7c66a825b3fbb28a51b56d1e13
Christian Bingman <42191425+ChristianBingman@users.noreply.github.com>
2020-07-10 20:32:03 -0500

    Added auto push

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -21,20 +21,20 @@ export class CardObj {
 export class ProjectsComponent implements OnInit {
 
   cardsFeatured: CardObj[] = [
-    new CardObj("../../assets/terminalSample.png", "Mini-Blockchain", "A blockchain class written in C++ with block and blockchain verification", "https://github.com/ChristianBingman/Mini-Blockchain"),
-    new CardObj("../../assets/terminalSample.png", "This Website", "The website you are on! This website created in Angular 9", "https://github.com/ChristianBingman/PortfolioWebsite"),
-    new CardObj("../../assets/terminalSample.png", "Graph Class", "A graph class used to create undirected graphs in C++ with weighted edges", "https://github.com/ChristianBingman/BasicGraph"),
-    new CardObj("../../assets/terminalSample.png", "Sorting Visualizer", "Visualizes several sorting algorithms in the terminal", "https://github.com/ChristianBingman/Sorting-Visualizer"),
-    new CardObj("../../assets/terminalSample.png", "UBIF Portal CSV Creator", "Uses your portal cookies to grab all workorders by the created by date and puts the data into a CSV", "https://github.com/ChristianBingman/UBIFtoCSV"),
+    new CardObj("assets/terminalSample.png", "Mini-Blockchain", "A blockchain class written in C++ with block and blockchain verification", "https://github.com/ChristianBingman/Mini-Blockchain"),
+    new CardObj("assets/terminalSample.png", "This Website", "The website you are on! This website created in Angular 9", "https://github.com/ChristianBingman/PortfolioWebsite"),
+    new CardObj("assets/terminalSample.png", "Graph Class", "A graph class used to create undirected graphs in C++ with weighted edges", "https://github.com/ChristianBingman/BasicGraph"),
+    new CardObj("assets/terminalSample.png", "Sorting Visualizer", "Visualizes several sorting algorithms in the terminal", "https://github.com/ChristianBingman/Sorting-Visualizer"),
+    new CardObj("assets/terminalSample.png", "UBIF Portal CSV Creator", "Uses your portal cookies to grab all workorders by the created by date and puts the data into a CSV", "https://github.com/ChristianBingman/UBIFtoCSV"),
   ];
 
   cardsAll: CardObj[] = [
-    new CardObj("../../assets/terminalSample.png", "Mini-Blockchain", "A blockchain class written in C++ with block and blockchain verification", "https://github.com/ChristianBingman/Mini-Blockchain"),
-    new CardObj("../../assets/terminalSample.png", "Graph Class", "A graph class used to create undirected graphs in C++ with weighted edges", "https://github.com/ChristianBingman/BasicGraph"),
-    new CardObj("../../assets/terminalSample.png", "Sorting Visualizer", "Visualizes several sorting algorithms in the terminal", "https://github.com/ChristianBingman/Sorting-Visualizer"),
-    new CardObj("../../assets/terminalSample.png", "UBIF Portal CSV Creator", "Uses your portal cookies to grab all workorders by the created by date and puts the data into a CSV", "https://github.com/ChristianBingman/UBIFtoCSV"),
-    new CardObj("../../assets/terminalSample.png", "Hashing", "Imports a CSV file and uses a hashtable to store the data to be able to easily sort through", "https://github.com/ChristianBingman/BasicHashing"),
-    new CardObj("../../assets/terminalSample.png", "Work-Play", "A program that finds possible conversions from one word to another using a dictionary from CS141", "https://github.com/ChristianBingman/Work-Play"),
+    new CardObj("assets/terminalSample.png", "Mini-Blockchain", "A blockchain class written in C++ with block and blockchain verification", "https://github.com/ChristianBingman/Mini-Blockchain"),
+    new CardObj("assets/terminalSample.png", "Graph Class", "A graph class used to create undirected graphs in C++ with weighted edges", "https://github.com/ChristianBingman/BasicGraph"),
+    new CardObj("assets/terminalSample.png", "Sorting Visualizer", "Visualizes several sorting algorithms in the terminal", "https://github.com/ChristianBingman/Sorting-Visualizer"),
+    new CardObj("assets/terminalSample.png", "UBIF Portal CSV Creator", "Uses your portal cookies to grab all workorders by the created by date and puts the data into a CSV", "https://github.com/ChristianBingman/UBIFtoCSV"),
+    new CardObj("assets/terminalSample.png", "Hashing", "Imports a CSV file and uses a hashtable to store the data to be able to easily sort through", "https://github.com/ChristianBingman/BasicHashing"),
+    new CardObj("assets/terminalSample.png", "Work-Play", "A program that finds possible conversions from one word to another using a dictionary from CS141", "https://github.com/ChristianBingman/Work-Play"),
   ];
 
 
